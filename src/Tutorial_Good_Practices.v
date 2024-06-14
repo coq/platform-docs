8fd2bd0efdc3a714e4ecaed46143f0c16b5b2bae
thomas-lamiaux <thomas.lamiaux@ens-paris-saclay.fr>
2024-06-14 18:06:04 +0200

    first commit

---
+++ src/Tutorial_Good_Practices.v
@@ -0,0 +1,24 @@
+(** * Tips and Good Practices
+
+    *** Summary
+
+        This tutorial gathers tips and good practices
+
+    *** Table of content
+
+        Todo
+
+
+    *** Prerequisites
+
+    Needed:
+    -
+
+    Not Needed:
+    -
+
+    Installation:
+  - It only requires having Coq installed
+
+*)
+
