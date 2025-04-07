db9d251e8d3e45700fa633129f6d001f46032e1e
Thomas Lamiaux <thomas.lamiaux@gmail.com>
2025-04-07 21:58:39 +0200

    add intro type tactics

--- src/Tutorial_Ltac2_backtracking.v
+++ src/Tutorial_Ltac2_backtracking.v
@@ -23,17 +23,22 @@
 
 *)
 
-(*
--> plus
--> once
--> once_plus
--> orelse (||) and ++
+(** Let us start by importing Ltac2, and write a small function for
+    printing the current goals to check out what is going on.
 *)
 
 From Ltac2 Require Import Ltac2.
 From Ltac2 Require Import Printf.
 From Ltac2 Require Import Notations.
 
+Ltac2 print_goals () :=
+  Control.enter ( fun _ =>
+  match! goal with
+  [ |- ?t] => printf "the goal is %t" t
+  end
+  ).
+
+Ltac2 Notation "print_goals" := print_goals ().
 
 Ltac2 only0 (startgoal : int) (endgoal : int option) (tac : unit -> 'a) : 'a :=
 let clamp i :=
@@ -48,12 +53,178 @@ Ltac2 Notation "only" startgoal(tactic) endgoal(opt(seq("-", tactic))) ":" tac(t
 only0 startgoal endgoal tac.
 
 
-    (* Reimplement || and + *)
+(** 1. Manipulating Tactics with Ltac2, and Thunks
 
-(* Let us code a "++" combinator that given two tactics :
--
--
--
+  Before explaining how to manipualte backtracking with Ltac2,
+  let us remind how to manipulate tactics with Ltac2 functions.
+
+  Tactics such as [assumption] or [left] modify the proof state through side effects,
+  such as modifying hypotheses or resolving an existential variable.
+  They do not produces Ltac2 value, like an integer [Int] of Ltac2, and are
+  hence of type [unit].
+
+  Opposite to Rocq that is call-by-name, Ltac2 is language that is call-by-value.
+  This means that a Ltac2 function evaluates its arguments before evaluating the body of the function.
+  Being call-by-value is important for a functional programming language like Ltac2
+  to be predicatable and intuitive.
+
+  However, this has an important side effect to manipulate tactics.
+  If we pass a tactic like [fail] **as is** to a Ltac2 function,
+  it will be evaluated first.
+
+  As [fail] fails, the whole Ltac2 tactic will fail, even if the tactic [fail]
+  is not used.
+  For instance, consider a function doing ignoring its arguments, and doing nothing.
+  Applying it directly to [fail] will fail.
+*)
+
+Ltac2 bad_ignore (bad_tac : unit) : unit := ().
+
+Goal 0 = 0.
+  Fail bad_ignore fail.
+Abort.
+
+(** Consequently, tactics must be "thunk" to be manipulated by a Ltac2 function.
+    That is, rather than manipulating [tac : unit] directly, a Ltac2 function
+    manipulates it is "thunk" version instead [fun _ => tac : unit -> unit].
+    As function are alrady fully-evaluated in call-by-value, "thunk" do
+    not get reduced further when passed as arguments of functions, and
+    can hence be evaluated on demand [(fun _ => tac) ()] by Ltac2 functions.
+
+    A [good_ignore] function, would hence be of the following type, and we can
+    check it works:
+*)
+
+Ltac2 good_ignore (tac : unit -> unit) : unit := ().
+
+Goal (0 = 0).
+  (* succeeds doing nothing  *)
+  good_ignore (fun _ => fail).
+Abort.
+
+(** While, this works, no one wants to write "thunk" by hand in proof script.
+    Thanfully, there is an easy not to have to. It suffices to define
+    a Ltac2 notation that directly inserts the thunk around tactics.
+    For instance, for [good_ignore] we declare a notation with the same
+    name, that takes a tactic as argument that is "thunk", i.e. [tac(thunk(tactic))].
+    It is then possible to write clean proof script as usual, while having a
+    meta-programming language with a clear semantics, opposite to Ltac1.
+*)
+
+Ltac2 Notation "good_ignore" tac(thunk(tactic)) := good_ignore tac.
+
+Goal 0 = 0.
+  good_ignore fail.
+Abort.
+
+
+
+
+
+
+(** ** 1. Backtracking with Ltac2
+
+    *** 1.1 Working in a Backtracking Monad
+
+    TODOT TEXT
+*)
+
+(** ** 1.2 Primitives to Handle Backtracking
+
+There are three simple but very powerful primitives to handle backtracking in Ltac2:
+
+- Control.zero : exn -> 'a
+- Control.plus : (unit -> 'a) -> (exn -> 'a) -> 'a
+- Control.case : (unit -> 'a) -> ('a * (exn -> 'a)) result
+
+
+
+*)
+
+
+
+(*
+-> plus
+-> once
+-> once_plus
+-> orelse (||) and ++
+*)
+
+
+(* 2.1 A Backtracking or *)
+
+(** To understand how [Control.plus] works, let us first consider how to use it
+    to code a [or] tactical that given two tactics [tac1 or tac2] will:
+    1. Execute [tac1] until tac1 produces a succes
+    2. If [tac1] produces no success, then it execute [tac2]
+    3. If [tac1 + tac2] succeeds but the value returned leads to a subsequent failure
+       when composed with another tactic, i.e. [tac1 + tac2 ; ... ; tac3],
+       then it backtracks to [tac1 + tac2] to find another success,
+       possibly in [tac2] if they are none left in [tac1].
+
+
+Defining an [or] that backtracks is a very direct application of [Control.plus]
+Given two tactics [tac1 tac2: unit -> unit], [or_backtrack]
+
+*)
+
+
+
+Ltac2 or_backtrack (tac1 : unit -> unit) (tac2 : unit -> unit) : unit :=
+ Control.plus tac1 (fun _ => tac2 ()).
+
+
+Ltac2 Notation tac1(thunk(self)) "++" tac2(thunk(self)) :=
+  or_backtrack tac1 (fun _ => tac2 ()).
+
+Goal exists n, n = 2 /\ n = 3.
+ unshelve econstructor; only 2 : split.
+ (* Apply the first tactic that succeeds *)
+ - (exact 2) ++ (exact 1).
+ - assumption ++ reflexivity.
+ (* Fails if none of the tactic succeeds *)
+ - Fail assumption ++ (exact 4).
+Abort.
+
+(* If a choice fails, then it will backtrack to the original choice
+*)
+Goal exists n, n = 2.
+ unshelve econstructor.
+ (* The first succes is [exact 1] which leads to the goal [1 = 2].
+    This latter leads to a failure as [reflexivity] can not solve the goal [1 = 2].
+    The proof script hence backtracks to apply [exact 2] which succeeds, then
+    run the proof script again which this time succeeds. *)
+ all: only 1: (exact 1) ++ (exact 2); print_goals; reflexivity.
+Qed.
+
+Goal exists n, n = 3.
+ unshelve econstructor.
+ (* The first succes is [exact 1] which leads to the goal [1 = 2].
+    This latter leads to a failure as [reflexivity] can not solve the goal [1 = 2].
+    The proof script hence backtracks to apply [exact 2] which succeeds, then
+    run the proof script again which this time succeeds. *)
+ all: only 1: ((exact 1) ++ (exact 2)) ++ (exact 3); print_goals; reflexivity.
+Qed.
+
+
+(** In practice this behaviour is not always wanted as it can make proof scripts
+    costly, or hard to predict.
+    For instance, suppose we have a script of the form [tac1 ++ tac2; script; tac4]
+    with [tac4] failing and causing batracking in [tac1 ++ tac2]:
+    - This can be costly as [script] will be repeated for every success
+      of [tac1 ++ tac2] until one is found that will make [tac4] succeed.
+    - This can cause an exponantial blow up of combinations tried if [script]
+      itsel can backtrack.
+    - Overall, it can make the proof script unpredicatable as during [script]
+      existential variables like [?n] can be resolved differently than expected,
+      or lemma applied differently than expected leading to a goal that can
+      no longer be solved.
+
+  We want instead to code a [||] tactical that for [tac1 || tac2] will
+  apply the first tactic that succeeds, but will not backtrack if the choice
+  leads to subsequent failure.
+
+  Comes in play the [Control.case]
 
 To do so we consider
 
@@ -74,6 +245,9 @@ Ltac2 orelse (tac1 : unit -> 'a) (tac2 : unit -> 'a) : 'a :=
   | Val (x,k) => Control.plus (fun _ => x) k
   end.
 
+  Ltac2 Notation tac1(thunk(self)) "||" tac2(thunk(self)) :=
+  orelse tac1 tac2.
+
 (** Because Lta2 is a call-by-value language, we must put tactic into a "thunk"
     before passing to [orelse], otherwise it would be evaluated on the fly,
     and all the work done by [orsele] by pointless.
@@ -83,16 +257,13 @@ Ltac2 orelse (tac1 : unit -> 'a) (tac2 : unit -> 'a) : 'a :=
     around our tactics.
 *)
 
-Ltac2 Notation "or||" tac1(thunk(tactic(6))) tac2(thunk(tactic(6))) :=
-  orelse tac1 tac2.
-
 Goal exists n, n = 2 /\ n = 3.
   unshelve econstructor; only 2 : split.
   (* Apply the first tactic that succeeds *)
-  - orelse (fun _ => exact 2) (fun _ => exact 1).
-  - orelse (fun _ => assumption) (fun _ => reflexivity).
+  - (exact 2) || (exact 1).
+  - assumption || reflexivity.
   (* Fails if none of the tactic succeeds *)
-  - Fail orelse (fun _ => assumption) (fun _ => exact 4).
+  - Fail assumption || (exact 4).
 Abort.
 
 (* However, [orlse] will not backtrack on the choice of the tactic to apply
@@ -102,46 +273,19 @@ Goal exists n, n = 2.
   unshelve econstructor.
   (* It applies [exact 1] as it is the first tactic to succeeds, which leads to
      [1 = 2], it then fails on [reflexivity] rather than backtracking to do [exact 2]. *)
-  Fail all: only 1: orelse (fun _ => exact 1) (fun _ => exact 2); reflexivity.
-
-
-
-(* Instead of [||], we could prefer the operator [+] that will backtrack on his
-   choice of tactic if the tactic later failed.
-
-To do so, we use Control.plus. This is the most direct use of [Control.plus].
-It install a backtracking point around [tac1], that is it
-1. Execute [tac1] until tac1 produces a value ???
-2. If [tac1] fails, it backtracks and execute [tac2]
-
- *)
-
-Ltac2 or_backtrack (tac1 : unit -> 'a) (tac2 : unit -> 'a) : 'a :=
-  Control.plus tac1 (fun _ => tac2 ()).
-
-Goal exists n, n = 2 /\ n = 3.
-  unshelve econstructor; only 2 : split.
-  (* Apply the first tactic that succeeds *)
-  - or_backtrack (fun _ => exact 2) (fun _ => exact 1).
-  - or_backtrack (fun _ => assumption) (fun _ => reflexivity).
-  (* Fails if none of the tactic succeeds *)
-  - Fail or_backtrack (fun _ => assumption) (fun _ => exact 4).
+  Fail all: only 1: (exact 1) || (exact 2); reflexivity.
 Abort.
 
-(* Opposite to [orelse], [or_backtrack] will backtrack on the choice in case of failure.
-*)
-Goal exists n, n = 2.
-  unshelve econstructor.
-  (* It applies [exact 1] as it is the first tactic to succeeds, which leads to
-     [1 = 2], it then fails on [reflexivity] rather than backtracking to do [exact 2]. *)
-  all: only 1: or_backtrack (fun _ => exact 1) (fun _ => exact 2); reflexivity.
 
-(* This is a key difference that one should always keep in mind when handling backtracking.
-   In particular, while [or_backtrack] will succeed on more goals, backtracking
-   like that can be expensive, particular if the tactics between the backtracking point
-   and the failure point are expensive.
-*)
 
+
+
+
+(** 2. Backtracking and Goal Focusing *)
+
+
+Goal (True \/ False) /\ (False \/ True).
+  Fail split; left ++ right; print_goals; econstructor.
 
 
     (* Reimplement ; *)
@@ -180,3 +324,5 @@ Abort.
 
 
     (* Reimplement only *)
+
+
